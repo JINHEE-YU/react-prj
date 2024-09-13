fa1658798bbce3730ba6103cc90f52fa33d68a84
JINHEE-YU <yujh2292@gmail.com>
2024-09-13 15:48:59 +0900

    ✨ Memo 레이아웃

--- src/app/pages/MemoPage/index.tsx
+++ src/app/pages/MemoPage/index.tsx
@@ -1,16 +1,27 @@
+import MemoEditor from 'app/components/Memo/Editor';
+import MemoList from 'app/components/Memo/List';
+import MemoToolBar from 'app/components/Memo/Toolbar';
 import * as React from 'react';
-import { Helmet } from 'react-helmet-async';
 import { useDispatch, useSelector } from 'react-redux';
+import styled from 'styled-components';
 
+const FlexRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 export function MemoPage() {
   const dispatch = useDispatch();
 
   return (
     <>
-      <Helmet>
-        <title>Main</title>
-        <meta name="description" content="Memo Main App" />
-      </Helmet>
+      <div>
+        <MemoToolBar />
+        <FlexRow>
+          <MemoList />
+          <MemoEditor />
+        </FlexRow>
+      </div>
     </>
   );
 }
